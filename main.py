6f08c5f72ddcaa5d80f1adb6c1ebb37294ffe151
vishal <vivevich3@gmail.com>
2022-10-29 19:35:05 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+def power(a,b):
+    return a**b
+def mod(a,b):
+    return a%b
+a=int(input("enter any number"))
+b=int(input("enter another number"))
+c=int(input("choice"))
+
+if c==1:
+    e=add(a,b)
+    print (e)
+
+elif c==2:
+    f=sub(a,b)
+    print (f)
+elif c==3:
+    g=mul(a,b)
+    print (g)
+elif c==4:
+    h=div(a,b)
+    print (h)
+elif c==5:
+    i=power(a,b)
+    print (i)
+elif c==6:
+    j=mod(a,b)
+    print (j)
+def append
+list=()
+
+
+
+
+
+
+
+
